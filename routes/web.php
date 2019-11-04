a3c89a8f322fc7f5e2b8ec9b62696f3a35fa573a
Sam1050 <47476775+Sam1050@users.noreply.github.com>
2019-11-04 17:22:13 +0300

    best fresh start

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+
+Route::post('/login/custom', [
+  'uses' => 'LoginController@login',
+   'as' => 'login.custom'
+]);
+
+
+Route::get('/', 'HomeController@index')->name('first');
+
+
+
+Route::group(['middleware' => 'auth'], function(){
+
+Route::get('/timetable', function () {
+     return View ('timetable');
+
+});
+
+Route::get('/venue', function () {
+     return View ('venue');
+
+});
+
+Route::get('/report', function () {
+     return View ('report');
+
+});
+
+});
