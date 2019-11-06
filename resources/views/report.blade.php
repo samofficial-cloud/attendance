b8ee9cc76e3845a88a3dabb1d21a7b792e8699ad
Sam1050 <47476775+Sam1050@users.noreply.github.com>
2019-11-06 11:19:12 +0300

    fixed select menu

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -40,92 +40,34 @@
                 <div class="card-body">
                   <form>
                     <div class="form-group row">
+                      <label for="" class="col-sm-4 col-form-label">For:</label>
+                      <select id="getFname" onchange="SelectCheck(this)">
+                         <option value="1">All students</option>
+                         <option value="2" id="Option">One student</option>
+                         <option value="3">Lecturer</option>
+                     </select>
+                  </div>
+
+
+
+  <div class="form-group row">
     <label for="inputEmail3" class="col-sm-4 col-form-label">Course ID:</label>
     <div class="col-sm-8">
       <input type="text" class="form-control" id="inputEmail3" placeholder="Course ID" required>
     </div>
   </div>
 
- <!--  ******************* -->
- <!-- <fieldset class="form-group"> -->
-    <div class="form-group row">
-      <legend class="col-form-label col-sm-5 pt-0">For:</legend>
-
-
-  <!-- <div class="form-row align-items-center col-sm-7">
-    <div class="col-auto my-1">
-      <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-        <option selected>Choose..</option>
-        <option value="1">Student</option>
-        <option value="2">Lecturer</option>
-      </select>
-    </div>
-  </div> -->
-  <head>
-
-    <script src=
-"https://code.jquery.com/jquery-1.12.4.min.js">
-    </script>
-    <style type="text/css">
-        .select {
-            color: #fff;
-            padding: 30px;
-            margin-top: 30px;
-            width: 100%;
-            background: green
-        }
-
-       /* label {
-            margin-right: 20px;
-        }*/
-    </style>
-</head>
-
-<div>
-
-            <label>
-                <input type="radio" name="colorCheckbox"
-                    value="D"> Lecturer </label>
-
-
-
-         <div>
-            <label>
-                <input type="radio" name="colorCheckbox"
-                    value="E">Student </label>
-
-        </div>
-        <div>
-            <label>
-                <input type="radio" name="colorCheckbox"
-                    value="C"> All Students </label>
-
-        </div>
-
-
-</div>
-
- <div class="D">
-
-
-        <div class="C">
-  <div class="form-group row">
-    <label for="inputEmail3" class="col-sm-5 col-form-label">Registration No:</label>
-    <div class="col-sm-7">
+<div id="DivCheck" style="display:none;">
+  <div class="form-group row" >
+    <label for="inputEmail3" class="col-sm-4 col-form-label">Registration No:</label>
+    <div class="col-sm-8">
       <input type="text" class="form-control" id="inputEmail3" placeholder="Registration No">
     </div>
   </div>
-  </div>
-
-  </div>
-
-
 </div>
 
 
 
-   <fieldset class="form-group">
     <div class="row">
       <legend class="col-form-label col-sm-2 pt-0">From:</legend>
 
@@ -179,7 +121,11 @@
   </div>
 
 </div>
-</fieldset>
+
+
+
+
+
  <center><button class="btn btn-primary" type="submit">Generate Report</button></center>
 </form>
 </div>
@@ -187,16 +133,24 @@
 </div>
 </div>
 </div>
+</div>
 
 <script type="text/javascript">
-            $(document).ready(function() {
-                $('input[type="radio"]').click(function() {
-                    var inputValue = $(this).attr("value");
-                    $("." + inputValue).toggle();
-
-                });
-            });
-        </script>
+function SelectCheck(nameSelect){
+  if(nameSelect){
+      OptionValue = document.getElementById("Option").value;
+      if(OptionValue == nameSelect.value){
+          document.getElementById("DivCheck").style.display = "block";
+      }
+      else{
+          document.getElementById("DivCheck").style.display = "none";
+      }
+  }
+  else{
+      document.getElementById("DivCheck").style.display = "none";
+  }
+}
+</script>
 
 
 @endsection
